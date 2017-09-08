38b0d3a99e95f2c47c943ec45aec8586e1a0919e
Allen Woods <allen.woods@foxmail.com>
2017-09-08 22:15:12 +0800

    Add GAN setting

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
+import os
+import numpy as np
+from keras.datasets import mnist
+from keras.layers import Input
+from keras import backend as K
+from keras.optimizers import Adam
+from keras.utils import to_categorical
 from src.nn import DCGAN_G, DCGAN_D
+from src.classifier import get_mnist_classifier
+from src.inception_score import get_inception_score
 
+from matplotlib import pyplot as plt
+import seaborn as sns
+sns.set(context='paper', style='white', palette='Set2', font_scale=1, color_codes=False,
+        rc={'font.family':'sans-serif', 'font.serif':['Palatino'], 'font.sans-serif':['DejaVu Sans'], 'text.usetex':True})
+
+# Load Datasets
+(mnist_x_train, mnist_y_train), (mnist_x_test, mnist_y_test) = mnist.load_data()
+mnist_data = np.concatenate([np.expand_dims(mnist_x_train, 1),
+                             np.expand_dims(mnist_x_test, 1)], axis=0)
+mnist_data = mnist_data.clip(0, 255)/255
+mnist_label = to_categorical(np.concatenate([mnist_y_train, mnist_y_test]), num_classes=10)
+
+X_train = mnist_data
+
+# Set up Networks
 nc = 1
 nz = 100
 ngf = 64
@@ -8,12 +32,89 @@ n_extra_layers = 0
 λ = 10
 
 img_size = 28
-batch_size = 64
+batch_size = 100
 lr_d = 1e-4
 lr_g = 1e-4
 
 netG = DCGAN_G(img_size, nz, nc, ngf)
 netD = DCGAN_D(img_size, nc, ndf, wgan=False)
 
-print(netG.summary())
-print(netD.summary())
+# Define optimization
+netD_real_input = Input(shape=(nc, img_size, img_size))
+noisev = Input(shape=(nz,))
+netD_fake_input = netG(noisev)
+
+ϵ_input = K.placeholder(shape=(None, 1, 1, 1))
+netD_mixed_input = Input(shape=(nc, img_size, img_size),
+                         tensor=ϵ_input * netD_real_input + (1 - ϵ_input) * netD_fake_input)
+
+loss_real = K.mean(netD(netD_real_input))
+loss_fake = K.mean(netD(netD_fake_input))
+
+grad_mixed = K.gradients(netD(netD_mixed_input), [netD_mixed_input])[0]  # only gradient values, no variables
+norm_grad_mixed = K.sqrt(K.sum(K.square(grad_mixed), axis=[1, 2, 3]))
+grad_penalty = K.mean(K.square(norm_grad_mixed - 1))
+
+d_loss = loss_fake-loss_real + λ * grad_penalty
+g_loss = -loss_fake
+
+# Define training function
+d_training_updates = Adam(lr=lr_d, beta_1=0.0, beta_2=0.9).get_updates(netD.trainable_weights,[],d_loss)
+netD_train = K.function([netD_real_input, noisev, ϵ_input],
+                        [loss_real, loss_fake, d_loss],
+                        d_training_updates)
+g_training_updates = Adam(lr=lr_g, beta_1=0.0, beta_2=0.9).get_updates(netG.trainable_weights,[], g_loss)
+netG_train = K.function([noisev], [g_loss], g_training_updates)
+
+# Define Runing Process
+inceps_score_log = []
+confidence_log = []
+class__log = []
+records = []
+niter = 100
+Diters = 5
+gen_iterations = 0
+errG = 0
+
+mnist_classifier_weights = os.path.join('weights', 'mnist_classifier_weights.h5')
+mnist_classifier = get_mnist_classifier(mnist_classifier_weights)
+
+real_data = mnist_data[np.random.choice(range(len(mnist_data)), size=batch_size)]
+
+fixed_noise = np.random.normal(size=(batch_size, nz)).astype('float32')
+for epoch in range(niter):
+    i = 0
+    #  每個 epoch 洗牌一下
+    np.random.shuffle(X_train)
+    batches = X_train.shape[0] // batch_size
+    while i < batches:
+        if gen_iterations < 25 or gen_iterations % 500 == 0:
+            _Diters = 100  # Pretrain D for 100 epochs
+        else:
+            _Diters = Diters
+        j = 0
+        while j < _Diters and i < batches:
+            j += 1
+            i += 1
+            real_data = X_train[np.random.choice(range(len(X_train)), size=batch_size)]
+            noise = np.random.normal(size=(batch_size, nz))
+            ϵ = np.random.rand(batch_size, 1, 1, 1)
+            errD_real, errD_fake, errD = netD_train([real_data, noise, ϵ])
+
+        if gen_iterations % 500 == 0:
+            print('[%d/%d][%d/%d][%d] Loss_G:%f \t Loss_D:%f \t penalty_factor:%f \t D_real:%f \t D_fake:%f \t'
+                  % (epoch, niter, i, batches, gen_iterations, errG, errD, penalty_fac, np.mean(errD_real),
+                     np.mean(errD_fake)))
+            fake = netG.predict(fixed_noise)
+            mean_confidence, class_entrophy, score_mean, score_std = get_inception_score(fake, mnist_classifier)
+
+            plt.imshow(merge_imgs(fake, 10, 10, transfrom=True))
+            plt.axis('off')
+            plt.
+
+        noise = np.random.normal(size=(batch_size, nz))
+        errG, = netG_train([noise])
+        gen_iterations += 1
+        score_mean, score_std = cifar10_evaluator(netG.predict(fixed_noise))
+        inceps_score.append((score_mean, score_std))
+        records.append([errG, errD, errD_real, errD_fake, penalty_fac])
